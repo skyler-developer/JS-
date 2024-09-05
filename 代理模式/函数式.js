907bdbebfb47484b1568e3e51930f814807a6815
skyler <skylerhaut@foxmail.com>
2024-09-05 18:20:49 +0800

    feat: 代理模式

---
+++ 代理模式/函数式.js
@@ -0,0 +1,23 @@
+var myImage = (function () {
+    var imgNode = document.createElement("img");
+    document.body.appendChild(imgNode);
+
+    return function (src) {
+        imgNode.src = src;
+    };
+})();
+
+var proxyImage = (function () {
+    var img = new Image();
+
+    img.onload = function () {
+        myImage(this.src);
+    };
+
+    return function (src) {
+        myImage("file:// /C:/Users/svenzeng/Desktop/loading.gif");
+        img.src = src;
+    };
+})();
+
+proxyImage("http://imgcache.qq.com/music// N/k/000GGDys0yA0Nk.jpg");
